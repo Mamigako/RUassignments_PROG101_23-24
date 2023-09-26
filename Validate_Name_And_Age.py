741a319de63c3635a1766d8d46d8f15cc8d20b24
Maximilian Klimko <82115185+Mamigako@users.noreply.github.com>
2023-09-26 22:34:59 +0000

    Name and Age validator

--- Validate_Name_And_Age.py
+++ Validate_Name_And_Age.py
@@ -1,13 +1,45 @@
-def main():
-    pass  # TODO: Complete the function
+import re
 
+age_valid = False
+name_valid = False
+
+def main():
+    global age_valid
+    global name_valid
+    
+    while not name_valid:
+        name = get_name()
+    
+    while not age_valid:
+        age = get_age()
+    
+    print(f"Nice to meet you {name}. Congratulations on your {age} years.")
 
 def get_name() -> str:
-    pass  # TODO: Complete the function
-
+    global name_valid
+    name = input("What's your name? ")
+    if re.search("[^a-zA-Z\s]", name):
+        print("Please enter a valid name.")
+    elif name == "":
+        print("Please enter a valid name.")
+    else:
+        name_valid = True
+        return name
 
 def get_age() -> str:
-    pass  # TODO: Complete the function
+    global age_valid
+    age = input("How old are you? ")
+    if not re.search("[0-9]", age):
+        print("Please enter an integer.")
+    else:
+        try:
+            if int(age) < 0 or int(age) > 125:
+                print(f"You seriously expect me to believe you are {age} years old?")
+            else:
+                age_valid = True
+                return str(age)
+        except ValueError:
+            print("Please enter an integer.")
 
 
 if __name__ == "__main__":
